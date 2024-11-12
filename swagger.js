c80a6c568be55da05bbd213257c237627dd3a272
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-11-12 22:37:41 +0530

    fixes

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = './src/utils/swagger-output.json';
-const routes = ['./server.js'];
+const routes = ['./index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
